31ba9a088a52e1db036c501c2110950f7d501319
ClemLy <GDClemLy@yahoo.com>
2023-12-06 15:40:45 +0100

    modification

--- src/astre/modele/Contrat.java
+++ src/astre/modele/Contrat.java
@@ -1,72 +1,44 @@
 package astre.modele;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Contrat
 {
-	private static List<Contrat> ensContrat = new ArrayList<> ( );
-	
 	private String nom;
 	private int    heureServiceContrat;
 	private int    heureMaxContrat;
 	private double ratioTP;
 
-	public Contrat ( String nom, int heureServiceContrat, int heureMaxContrat, double ratioTP )
+	public Contrat(String nom, int heureServiceContrat, int heureMaxContrat, double ratioTP)
 	{
 		this.nom                 = nom;
 		this.heureServiceContrat = heureServiceContrat;
 		this.heureMaxContrat     = heureMaxContrat;
 		this.ratioTP             = ratioTP;
-
-		Contrat.ensContrat.add ( this );
 	}
 
-	public static List<Contrat> getContrats ( ) { return Contrat.ensContrat;  }
+	public String getNom (            ) { return nom;     }
+	public void   setNom ( String nom ) { this.nom = nom; }
 
-	public String  getNom                 ( ) { return nom;                 }
-	public int     getHeureServiceContrat ( ) { return heureServiceContrat; }
-	public int     getHeureMaxContrat     ( ) { return heureMaxContrat;     }
-	public double  getRatioTP             ( ) { return ratioTP;             }
-	
+	public int  getHeureServiceContrat (                         ) { return heureServiceContrat;                     }
 	public void setHeureServiceContrat ( int heureServiceContrat ) { this.heureServiceContrat = heureServiceContrat; }
-	public void setNom                 ( String nom              ) { this.nom = nom;                                 }
-	public void setHeureMaxContrat     ( int heureMaxContrat     ) { this.heureMaxContrat = heureMaxContrat;         }
-	public void setRatioTP             ( double ratioTP          ) { this.ratioTP         = ratioTP;                 }
 
-	public boolean retirerListe ( )
+
+	public int  getHeureMaxContrat (                     ) { return heureMaxContrat;                 }
+	public void setHeureMaxContrat ( int heureMaxContrat ) { this.heureMaxContrat = heureMaxContrat; }
+
+	public double getRatioTP (                ) { return ratioTP;         }
+	public void   setRatioTP ( double ratioTP ) { this.ratioTP = ratioTP; }
+
+
+	public String toString ()
 	{
-		if ( ! Contrat.ensContrat.contains ( this ) ) return false;
-		
-		Contrat.ensContrat.remove ( this );
-		return true;
-	}
+		String sRet = "";
 
-	public String toString ( )
-	{
-		return String.format ( "Nom : %-20s - "                 , this.nom                 ) +
-		       String.format ( "Heure Service Contrat : %02d - ", this.heureServiceContrat ) +
-		       String.format ( "Heure Max Contrat : %02d - "    , this.heureMaxContrat     ) +
-		       String.format ( "Ratio TP : %,.2f"               , this.ratioTP             );
+		sRet = String.format ("Nom : %-20s - ",                    this.nom                                   ) +
+			   String.format ( "Heure Service Contrat : %02d - " , this.heureServiceContrat ) +
+			   String.format ( "Heure Max Contrat : %02d - " ,     this.heureMaxContrat        ) +
+			   String.format ( "Ratio TP : %,.2f",                 this.ratioTP           );
 
-	}
-
-	private static void test ( )
-	{
-		System.out.println ( Contrat.ensContrat );
-
-		Contrat c = new Contrat ( "test", 0, 0, 0 );
-
-		System.out.println ( Contrat.ensContrat );
-
-		c.retirerListe ( );
-
-		System.out.println ( Contrat.ensContrat );
-	}
-
-	public static void main(String[] args)
-	{
-		Contrat.test ( );
+		return sRet;
 	}
 	
 }
