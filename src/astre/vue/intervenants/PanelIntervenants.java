d13571e3f4239e37fb73a2ce2e6ac0e3dfd9041e
Matéo <129748653+MatKim76@users.noreply.github.com>
2023-12-06 20:29:54 +0100

    Ajout du panel et frame pour intervenants
    ya pas les packetages ni les references vers le tableau

---
+++ src/astre/vue/intervenants/PanelIntervenants.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+
+public class PanelIntervenants extends JPanel implements ActionListener
+{
+	private Tableau tableau;
+	
+	private JButton btnAjouter;
+	private JButton btnSupprimer;
+	
+	private JButton btnEnregistrer;
+	private JButton btnAnnuler;
+	
+	/**
+	 * 	Panel pour la frame des intervenants..
+	 * @author Matéo
+	 */
+	public PanelIntervenants ( )
+	{
+		this.setLayout( new BorderLayout() );
+		int marginSize = 10;
+		this.setBorder(BorderFactory.createEmptyBorder(marginSize, marginSize, marginSize, marginSize));
+		
+		//création des composants
+		String[] noms = { "Catégorie", "Nom", "Prénom", "hServ", "hMax", "Coef TP", "S1", "S3", "S5", "sTot", "S2", "S4", "S6", "sTot", "Total" };
+		this.tableau = new Tableau(noms);
+		repaint();
+		this.btnAjouter = new JButton( "Ajouter" );
+		this.btnSupprimer = new JButton( "Supprimer" );
+		this.btnEnregistrer = new JButton( "Enregistrer" );
+		this.btnAnnuler = new JButton( "Annuler" );
+		
+		JPanel panelCentre = new JPanel();
+		panelCentre.setLayout( new BorderLayout() );
+		panelCentre.setBorder(BorderFactory.createEmptyBorder(marginSize, marginSize, marginSize, marginSize));
+		
+		JPanel panelCentre2 = new JPanel();
+		JPanel panelSud = new JPanel();
+		
+		JScrollPane scrollPane = new JScrollPane(this.tableau);
+		
+		//Ajout des composants
+		panelCentre2.add( this.btnAjouter);
+		panelCentre2.add( this.btnSupprimer);
+		
+		panelCentre.add(panelCentre2, BorderLayout.SOUTH );
+		panelCentre.add(scrollPane, BorderLayout.CENTER);
+		
+		panelSud.add( this.btnEnregistrer );
+		panelSud.add( this.btnAnnuler );
+		
+		this.add( new JLabel( "Liste des intervenants" ), BorderLayout.NORTH );
+		this.add( panelCentre, BorderLayout.CENTER );
+		this.add( panelSud, BorderLayout.SOUTH );
+		
+		//met les actionListener
+		this.btnAjouter.addActionListener(this);
+		this.btnSupprimer.addActionListener(this);
+		this.btnEnregistrer.addActionListener(this);
+		this.btnAnnuler.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if( e.getSource() == this.btnAjouter )
+		{
+			this.tableau.ajouterLigne();
+			this.repaint();
+		}
+		
+		if( e.getSource() == this.btnSupprimer )
+		{
+			this.tableau.supprimerLigne();
+			this.repaint();
+		}
+		
+		if( e.getSource() == this.btnEnregistrer )
+		{
+			
+		}
+		
+		if( e.getSource() == this.btnAnnuler )
+		{
+			
+		}
+	}
+}
