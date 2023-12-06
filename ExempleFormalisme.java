0b99704405fa7df3917a64bd10aaf1536d1b2d10
Maximilien Lesterlin <lesterlinpro@gmail.com>
2023-12-06 09:14:26 +0100

    Ajout du formalisme

---
+++ ExempleFormalisme.java
@@ -0,0 +1,43 @@
+public class ExempleFormalisme
+{
+	private static int[] tab;
+
+	private int arg;
+
+	public ExempleFormalisme ( int arg )
+	{
+		// Tous les cas qui sont faux
+		if ( test == true )
+		{
+			return;
+		}
+
+		// Code a éxécuter si les tests sont réussis
+	}
+
+	/*---------------------------------------*/
+	/*                GETTEUR                */
+	/*---------------------------------------*/
+
+	private int getArg ( ) { return this.arg; }
+
+	/*---------------------------------------*/
+	/*                SETTEUR                */
+	/*---------------------------------------*/
+
+	private void setArg ( int valeur ) { this.arg = valeur; }
+
+	/*---------------------------------------*/
+	/*                TESTEUR                */
+	/*---------------------------------------*/
+
+	private int estArg ( ) { return this.arg == arg; }
+
+	// On ne retourne pas le metier, on fait le lien dans le controleur
+	private boolean estMetier ( ) { return this.metier.test ( ); }
+
+	/*---------------------------------------*/
+	/*                TESTEUR                */
+	/*---------------------------------------*/
+
+}
