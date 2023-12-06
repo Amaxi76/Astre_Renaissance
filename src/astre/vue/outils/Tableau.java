e49a92e1ab85a77dd10de085ab25971d68a2bb01
Matéo <129748653+MatKim76@users.noreply.github.com>
2023-12-06 20:31:03 +0100

    Ajouts tablo et son modele yay

---
+++ src/astre/vue/outils/Tableau.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.Component;
+import javax.swing.table.*;
+
+/**
+ * Classe représentant un tableau personnalisable.
+ * @author Matéo
+ */
+public class Tableau extends JTable
+{
+	private String[] nomColonnes;
+	//private DefaultTableModel modele;
+	private ModeleTableau modele;
+	
+	public Tableau ( String[] nomColonnes )
+	{
+		this.nomColonnes = nomColonnes;
+		//this.modele = new DefaultTableModel ( this.nomColonnes, 1 );
+		this.modele = new ModeleTableau(this.nomColonnes);
+		
+		this.setModel ( this.modele );
+		//this.modele.addTableModelListener(this);
+		this.setSelectionMode ( ListSelectionModel.SINGLE_SELECTION );
+		
+		//Permet au tableau de prendre toute la frame
+		this.setAutoResizeMode ( JTable.AUTO_RESIZE_ALL_COLUMNS );
+		//Permet d'enpecher de déplacer les cases (je crois)(ne marche pas vraiment)
+		this.setDragEnabled ( false );
+
+	}
+	
+	/**
+	* Ajuste la taille des colonnes.
+	*/
+	private void ajusterTailleColonnes ( )
+	{
+		TableColumnModel columnModel = this.getColumnModel ( );
+		for ( int i = 0; i < columnModel.getColumnCount ( ); i++ )
+		{
+			int maxWidth = 0;
+			for ( int j = 0; j < this.getRowCount ( ); j++ )
+			{
+				TableCellRenderer cellRenderer = this.getCellRenderer ( j, i );
+				Object value = this.getValueAt ( j, i );
+				Component cellRendererComponent = cellRenderer.getTableCellRendererComponent ( this, value, false, false, j, i );
+				maxWidth = Math.max ( maxWidth, cellRendererComponent.getPreferredSize ( ).width );
+			}
+			columnModel.getColumn ( i ).setPreferredWidth ( maxWidth - 30 );
+		}
+	}
+	
+	/**
+	* Ajoute une ligne vide au tableau.
+	*/
+	public void ajouterLigne ( )
+	{
+		//this.modele.addRow ( new Object[ this.nomColonnes.length ] );
+		this.modele.ajouterLigne();
+		this.ajusterTailleColonnes ( );
+	}
+	
+	/**
+	* Supprime la ligne sélectionnée du tableau.
+	*/
+	public void supprimerLigne ( )
+	{
+		int selection = this.getSelectedRow ( );
+		if ( selection != -1 )
+		{
+			//this.modele.removeRow ( selection );
+			this.modele.supprimerLigne(selection);
+		}
+		this.ajusterTailleColonnes ( );
+	}
+
+}
