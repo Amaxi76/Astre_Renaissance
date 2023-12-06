5c4bab77aedd4b33fd01f74885e0b1140f72206f
ClemLy <GDClemLy@yahoo.com>
2023-12-06 15:55:14 +0100

    Modification de FramePrevisionnel.java

--- src/astre/vue/previsionnel/FramePrevisionnel.java
+++ src/astre/vue/previsionnel/FramePrevisionnel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import astre.Controleur;
 
@@ -40,5 +41,46 @@ public class FramePrevisionnel extends JFrame implements ActionListener
 
 		this.setSize     ( 1000, 700 );
 		this.setTitle    ( "Prévisionnel"   );
+
+		/* ------------------------- */
+		/* Création des composants   */
+		/* ------------------------- */
+
+		JPanel panelButton = new JPanel(new GridLayout(1, 5 ));
+
+		this.btncreerRessource = new JButton ( "créer Ressource"   );
+		this.btncreerSAE       = new JButton ( "créer SAÉ"         );
+		this.btncreerStage     = new JButton ( "créer Stage/Suivi" );
+		this.btnModifier       = new JButton ( "modifier"          );
+		this.btnSupprimer      = new JButton ( "supprimer"         );
+
+		panelButton.add ( this.btncreerRessource );
+		panelButton.add ( this.btncreerSAE       );
+		panelButton.add ( this.btncreerStage     );
+		panelButton.add ( this.btnModifier       );
+		panelButton.add ( this.btnSupprimer      );
+
+		/* ------------------------- */
+		/* Activation des composants */
+		/* ------------------------- */
+
+		this.btncreerRessource.addActionListener(this);
+		this.btncreerSAE      .addActionListener(this);
+		this.btncreerStage    .addActionListener(this);
+		this.btnModifier      .addActionListener(this);
+		this.btnSupprimer     .addActionListener(this);
+
+
+		this.setDefaultCloseOperation ( EXIT_ON_CLOSE );
+		this.setVisible               ( true        );
+	}
+
+	/* ActionListener */
+	public void actionPerformed ( ActionEvent e )
+	{
+		if ( e.getSource ( ) == this.btncreerRessource )
+		{
+			System.out.println("créerRessource");
+		}
 	}
 }
