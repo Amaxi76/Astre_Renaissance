b019d4d6457765474939c97d9f7688f3b7eb1551
Maximilien Lesterlin <lesterlinpro@gmail.com>
2023-12-06 09:39:00 +0100

    Lancement de l'accueil

--- src/astre/Controleur.java
+++ src/astre/Controleur.java
@@ -1,5 +1,18 @@
 package astre;
+
+import astre.vue.*;
+
 public class Controleur
 {
+	private FrameAccueil ihm;
+	
+	public Controleur ( )
+	{
+		this.ihm = new FrameAccueil ( this );
+	}
 
+	public static void main ( String[] args )
+	{
+		new Controleur ( );
+	}
 }
