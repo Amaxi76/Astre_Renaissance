899214e48c2f9ae4e862407b41f27a3804bc8286
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-12-06 16:49:08 +0100

    Onglets pour les semestres

---
+++ src/astre/vue/previsionnel/PanelEnsSemestre.java
@@ -0,0 +1,33 @@
+package astre.vue.previsionnel;
+
+import astre.Controleur;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import java.awt.*;
+
+/** Classe PanelEnsSemestre
+  * @author : Maximeuuu
+  * @version : 1.0 le 6/12/23
+  */
+public class PanelEnsSemestre extends JTabbedPane
+{
+	private Controleur ctrl;
+
+	public static final Dimension DIMENSION_TAB = new Dimension ( 350,400 );
+	private static final int NB_SEMESTRE = 6;
+
+	public PanelEnsSemestre ( Controleur ctrl )
+	{
+		// Configuration
+		this.ctrl = ctrl;
+		this.setPreferredSize ( PanelSelection.DIMENSION_TAB );
+		this.setUI ( new CustomTabbedPaneUI ( ) );
+		this.setTabLayoutPolicy ( JTabbedPane.SCROLL_TAB_LAYOUT );
+
+		// Creation et positionnement des composants
+		for ( int cptSemestre=1; cptSemestre<NB_SEMESTRE+1; cptSemestre++ )
+		{
+			this.add ( "S"+cptSemestre, new PanelSemestre ( cptSemestre, this.ctrl ) );
+		}
+	}
+}
