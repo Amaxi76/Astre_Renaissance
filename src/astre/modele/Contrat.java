bb8f7b34ebfd6ad09f1d96f39c7515df4e56a265
Maximilien Lesterlin <lesterlinpro@gmail.com>
2023-12-06 11:01:13 +0100

    Ajout de la liste des contrats

--- src/astre/modele/Contrat.java
+++ src/astre/modele/Contrat.java
@@ -1,44 +1,46 @@
 package astre.modele;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Contrat
 {
+	private static List<Contrat> ensContrat = new ArrayList<> ( );
+	
 	private String nom;
 	private int    heureServiceContrat;
 	private int    heureMaxContrat;
 	private double ratioTP;
 
-	public Contrat(String nom, int heureServiceContrat, int heureMaxContrat, double ratioTP)
+	public Contrat ( String nom, int heureServiceContrat, int heureMaxContrat, double ratioTP )
 	{
 		this.nom                 = nom;
 		this.heureServiceContrat = heureServiceContrat;
 		this.heureMaxContrat     = heureMaxContrat;
 		this.ratioTP             = ratioTP;
+
+		Contrat.ensContrat.add ( this );
 	}
 
-	public String getNom (            ) { return nom;     }
-	public void   setNom ( String nom ) { this.nom = nom; }
+	public static List<Contrat> getContrats ( ) { return Contrat.ensContrat;  }
 
-	public int  getHeureServiceContrat (                         ) { return heureServiceContrat;                     }
+	public String  getNom                 ( ) { return nom;                 }
+	public int     getHeureServiceContrat ( ) { return heureServiceContrat; }
+	public int     getHeureMaxContrat     ( ) { return heureMaxContrat;     }
+	public double  getRatioTP             ( ) { return ratioTP;             }
+	
 	public void setHeureServiceContrat ( int heureServiceContrat ) { this.heureServiceContrat = heureServiceContrat; }
+	public void setNom                 ( String nom              ) { this.nom = nom;                                 }
+	public void setHeureMaxContrat     ( int heureMaxContrat     ) { this.heureMaxContrat = heureMaxContrat;         }
+	public void setRatioTP             ( double ratioTP          ) { this.ratioTP         = ratioTP;                 }
 
-
-	public int  getHeureMaxContrat (                     ) { return heureMaxContrat;                 }
-	public void setHeureMaxContrat ( int heureMaxContrat ) { this.heureMaxContrat = heureMaxContrat; }
-
-	public double getRatioTP (                ) { return ratioTP;         }
-	public void   setRatioTP ( double ratioTP ) { this.ratioTP = ratioTP; }
-
-
-	public String toString ()
+	public String toString ( )
 	{
-		String sRet = "";
+		return String.format ( "Nom : %-20s - "                 , this.nom                 ) +
+		       String.format ( "Heure Service Contrat : %02d - ", this.heureServiceContrat ) +
+		       String.format ( "Heure Max Contrat : %02d - "    , this.heureMaxContrat     ) +
+		       String.format ( "Ratio TP : %,.2f"               , this.ratioTP             );
 
-		sRet = String.format ( "Nom : %-20s - ",                   this.nom                 ) +
-			   String.format ( "Heure Service Contrat : %02d - ",  this.heureServiceContrat ) +
-			   String.format ( "Heure Max Contrat : %02d - ",      this.heureMaxContrat     ) +
-			   String.format ( "Ratio TP : %,.2f",                 this.ratioTP             );
-
-		return sRet;
 	}
 	
 }
--- src/astre/modele/Intervenant.java
+++ src/astre/modele/Intervenant.java
@@ -4,6 +4,7 @@ public class Intervenant
 {
 	private String nom;
 	private String prenom;
+	private Contrat statut;
 	private double coefTp;
 	private int    service;
 	private int    heureMaximum;
