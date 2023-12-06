9c77295105e61834557102dc12e912cdc220b165
ClemLy <GDClemLy@yahoo.com>
2023-12-06 16:42:07 +0100

    Modification de FramePrevisionnel

--- src/astre/vue/previsionnel/FramePrevisionnel.java
+++ src/astre/vue/previsionnel/FramePrevisionnel.java
@@ -1,31 +1,23 @@
 package astre.vue.previsionnel;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 import astre.Controleur;
 
-public class FramePrevisionnel extends JFrame implements ActionListener
+public class FramePrevisionnel extends JFrame
 {
 	/*-------------*/
 	/*--Attributs--*/
 	/*-------------*/
 
-	/** Un Controleur pour pouvoir accéder au controleur
-	 * 
-	 */
-	private Controleur  ctrl;
-
-	private JButton btncreerRessource;
-	private JButton btncreerSAE;
-	private JButton btncreerStage;
-	private JButton btnModifier;
-	private JButton btnSupprimer;
+	private Controleur ctrl;
+	
+	private JPanel panelPrevisionnel;
 
 	/*----------------*/
 	/*--Constructeur--*/
@@ -35,7 +27,7 @@ public class FramePrevisionnel extends JFrame implements ActionListener
 	 * @param ctrl le controleur
 	 * 
 	 */
-	
+
 	public FramePrevisionnel ( Controleur ctrl )
 	{
 		this.ctrl = ctrl;
@@ -47,41 +39,12 @@ public class FramePrevisionnel extends JFrame implements ActionListener
 		/* Création des composants   */
 		/* ------------------------- */
 
-		JPanel panelButton = new JPanel(new GridLayout(1, 5 ));
+		panelPrevisionnel = new JPanel(new BorderLayout());
 
-		this.btncreerRessource = new JButton ( "créer Ressource"   );
-		this.btncreerSAE       = new JButton ( "créer SAÉ"         );
-		this.btncreerStage     = new JButton ( "créer Stage/Suivi" );
-		this.btnModifier       = new JButton ( "modifier"          );
-		this.btnSupprimer      = new JButton ( "supprimer"         );
-
-		panelButton.add ( this.btncreerRessource );
-		panelButton.add ( this.btncreerSAE       );
-		panelButton.add ( this.btncreerStage     );
-		panelButton.add ( this.btnModifier       );
-		panelButton.add ( this.btnSupprimer      );
-
-		/* ------------------------- */
-		/* Activation des composants */
-		/* ------------------------- */
-
-		this.btncreerRessource.addActionListener(this);
-		this.btncreerSAE      .addActionListener(this);
-		this.btncreerStage    .addActionListener(this);
-		this.btnModifier      .addActionListener(this);
-		this.btnSupprimer     .addActionListener(this);
+		this.add(panelPrevisionnel);
 
 
 		this.setDefaultCloseOperation ( EXIT_ON_CLOSE );
 		this.setVisible               ( true        );
-	}
-
-	/* ActionListener */
-	public void actionPerformed ( ActionEvent e )
-	{
-		if ( e.getSource ( ) == this.btncreerRessource )
-		{
-			System.out.println("créerRessource");
-		}
 	}
 }
