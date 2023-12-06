d41bdad7de6f2362baaff727f0ef44e91e65fde6
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-12-06 10:41:02 +0100

    menu bar

---
+++ src/astre/vue/outils/MenuBarAstre.java
@@ -0,0 +1,164 @@
+package astre.vue.outils;
+
+import javax.swing.*;
+import java.awt.Image;
+import java.awt.event.*;
+
+/**
+ * @author Maxime
+ */
+public class MenuBarAstre extends JMenuBar implements ActionListener
+{
+	private static final String REPERTOIRE = "../../../../data/images/";
+	
+	//Position des éléments d'un menu dans le "modeleBar"
+	private final int TYPE = 0;
+	private final int NAME = 1;
+	private final int ICON = 2;
+	private final int CHAR = 3;
+	private final int KEYS = 4;
+
+	public MenuBarAstre ( )
+	{
+		//Initialisation
+		super ( );
+		JMenu menuEnCreation = null;
+
+		//Format du MenuBar
+		String[][] modeleBar = MenuBarAstre.getModeleBar ( );
+		String hotkey;
+
+		for ( int cptLig = 0; cptLig < modeleBar.length; cptLig++ )
+		{
+			switch ( modeleBar[cptLig][TYPE] )
+			{
+				case "M":
+					menuEnCreation = this.creerMenu ( modeleBar[cptLig][NAME], modeleBar[cptLig][ICON], modeleBar[cptLig][CHAR] );
+					this.add ( menuEnCreation );
+					break;
+
+				case "I":
+					if ( modeleBar[cptLig].length-1 == KEYS ) { hotkey = modeleBar[cptLig][KEYS]; }
+					else { hotkey = null; }
+					menuEnCreation.add ( this.creerMenui ( modeleBar[cptLig][NAME], modeleBar[cptLig][ICON], modeleBar[cptLig][CHAR], hotkey ) );
+					break;
+
+				case "S":
+					menuEnCreation.addSeparator ( );
+			}
+		}
+	}
+
+	/**
+	 * Simplification de la création d'un élément Menu (correspond au premier niveau)
+	 */
+	public JMenu creerMenu ( String nom, String image, String mnemo )
+	{
+		JMenu menuTmp = new JMenu ( nom );
+
+		if ( !image.equals ( "" ) )
+		{
+			menuTmp.setIcon ( genererIcone( image, 20 ) );
+		}
+
+		menuTmp.setMnemonic ( mnemo.charAt( 0 ) );
+		return menuTmp;
+	}
+
+	/**
+	 * Simplification de la création d'un élément MenuItem (correspond au sous-niveau)
+	 */
+	public JMenuItem creerMenui ( String nom, String image, String mnemo, String hotkey )
+	{
+		JMenuItem menui = new JMenuItem ( nom );
+
+		if ( !image.equals ( "" ) )
+		{
+			menui.setIcon ( genererIcone ( image, 20 ) );
+		}
+
+		menui.setMnemonic ( mnemo.charAt ( 0 ) );
+
+		if( hotkey != null )
+		{
+			menui.setAccelerator( getEquivalentKeyStroke ( hotkey ) );
+		}
+
+		menui.addActionListener ( this );
+		return menui;
+	}
+
+	/**
+	 * Détection des évènements sur le JMenuBar
+	 */
+	public void actionPerformed ( ActionEvent e )
+	{
+		//A décomenter lorsque l'on aura un lien vers les autres pages
+		/*if ( e.getSource ( ) instanceof JMenuItem )
+		{
+			String nom = ( ( JMenuItem ) e.getSource () ).getText ( );
+
+			switch ( nom )
+			{
+				case "Semestres" 	-> this.ctrl.afficherSemestres ( );
+				case "Enseignants" 	-> this.ctrl.afficherEnseignants ( );
+				case "Modules" 		-> this.ctrl.afficherModules ( );
+			}
+		}*/
+	}
+
+	/**
+	 * Méthode utilitaire permettant de convertir une chaine de caractère en KeyStroke pour les raccourcis
+	 */
+	public static KeyStroke getEquivalentKeyStroke ( String hotkey )
+	{
+		String[] setTmp = hotkey.split ( "\\+" );
+		String sTmp="";
+
+		for ( int cpt=0; cpt<setTmp.length-1; cpt++ )
+		{
+			sTmp += setTmp[cpt].toLowerCase ( ) + " ";
+		}
+		sTmp += setTmp[ setTmp.length-1 ];
+
+		return KeyStroke.getKeyStroke ( sTmp );
+	}
+
+	/**
+	 * Méthode utilitaire permettant de générer une image redimensionnée
+	 */
+	public static ImageIcon genererIcone ( String image, int taille )
+	{
+		String REPERTOIRE = "imagesMenu/";
+		ImageIcon icone = new ImageIcon ( REPERTOIRE + image );
+		Image imgDim = icone.getImage ( ).getScaledInstance ( taille, taille, Image.SCALE_SMOOTH );
+		return new ImageIcon ( imgDim );
+	}
+
+	/**
+	 * Méthodes modifiable très facilement pour ajuster les éléments du MenuBar
+	 */
+	public static String[][] getModeleBar ( )
+	{
+		return new String[][] {
+			{ "M", "Gestion",		"gestion.png",		"G"				},
+			{ "I", "Semestres",		"semestres.png",	"S",			},
+			{ "I", "Enseignants" ,	"enseignants.png",	"E",			},
+			{ "I", "Modules",		"modules.png",		"M"				},
+			{ "S"														},
+			{ "I", "Horaires",		"horaires.png",		"H",			},
+
+			{ "M", "Aperçu",		"apercu.png",		"A"				},
+			{ "I", "Enseignants",	"",					"E",			},
+			{ "I", "Modules" ,		"",					"M", 			},
+			{ "I", "Semestres",		"",					"S"				},
+			{ "S"														},
+			{ "I", "Général",		"",					"G", 			},
+
+			{ "M", "Exportation",	"exportation.png",	"E"				},
+			{ "I", "Format PDF",	"pdf.png",			"P", "CTRL+P"	},
+			{ "I", "Format HTML" ,	"html.png",			"H", 			},
+								};
+	}
+
+}
--- src/astre/vue/outils/PanelDiagramme.java
+++ src/astre/vue/outils/PanelDiagramme.java
@@ -1,3 +1,5 @@
+package astre.vue.outils;
+
 // General
 import java.util.*;
 
@@ -20,95 +22,98 @@ import org.jfree.chart.labels.*;
  */
 public class PanelDiagramme extends JPanel
 {
-	private Map donnees;
-	private Color COULEUR_DEFAUT = Color.GRAY;
+	private static final Color COULEUR_DEFAUT = Color.GRAY;
 	
-	public PanelDiagramme( Map donnees, Color couleurMin, Color couleurMax )
+	private Map donnees;
+	
+	public PanelDiagramme ( Map donnees, Color couleurMin, Color couleurMax )
 	{
 		this.donnees = donnees;
-		this.setSize( new Dimension(800,500) );
+		this.setSize( new Dimension ( 800,500 ) );
 		
 		//Ajout des données
-		final DefaultPieDataset pieDataset = new DefaultPieDataset();
+		final DefaultPieDataset pieDataset = new DefaultPieDataset ( );
 		
-		Iterator iterator = this.donnees.entrySet().iterator();
-		while (iterator.hasNext())
+		Iterator iterator = this.donnees.entrySet ( ).iterator ( );
+		while ( iterator.hasNext ( ) )
 		{
-			Map.Entry mapentry = (Map.Entry) iterator.next();
-			pieDataset.setValue((String)mapentry.getKey(), (Double)mapentry.getValue());
+			Map.Entry mapentry = ( Map.Entry ) iterator.next ( );
+			pieDataset.setValue ( ( String ) mapentry.getKey ( ), ( Double ) mapentry.getValue ( ) );
 		}
 		
-		final JFreeChart pieChart = ChartFactory.createPieChart("Répartitions des heures de l'enseignant X", pieDataset, true, true, true);
-		final ChartPanel cPanel = new ChartPanel(pieChart);
+		final JFreeChart pieChart = ChartFactory.createPieChart ( "Répartitions des heures de l'enseignant X", pieDataset, true, true, true );
+		final ChartPanel cPanel = new ChartPanel ( pieChart );
 		
 		// Ajout des couleurs
-		PiePlot plot = (PiePlot) pieChart.getPlot();
+		PiePlot plot = ( PiePlot ) pieChart.getPlot ( );
 		
-		int nbDonnes = donnees.size();
-		List<Color> degrade = generateGradientColors(couleurMin, couleurMax, nbDonnes);
+		int nbDonnes = donnees.size ( );
+		List<Color> degrade = generateGradientColors ( couleurMin, couleurMax, nbDonnes );
 		int cptD=0;
-		iterator = this.donnees.entrySet().iterator();
-		while (iterator.hasNext())
+		iterator = this.donnees.entrySet ( ).iterator ( );
+		while ( iterator.hasNext ( ) )
 		{
-			Map.Entry mapentry = (Map.Entry) iterator.next();
+			Map.Entry mapentry = ( Map.Entry ) iterator.next ( );
 			
-			if( cptD == nbDonnes-1 ){ plot.setSectionPaint((String)mapentry.getKey(), COULEUR_DEFAUT); }
+			if ( cptD == nbDonnes-1 ) { plot.setSectionPaint ( ( String ) mapentry.getKey ( ), COULEUR_DEFAUT ); }
 			else
 			{
-				plot.setSectionPaint((String)mapentry.getKey(), degrade.get(cptD));
+				plot.setSectionPaint ( ( String ) mapentry.getKey ( ), degrade.get ( cptD ) );
 			}
 			cptD++;
 		}
 
-		plot.setShadowPaint(null);
-		plot.setBackgroundAlpha( 0.0f );
-		plot.setSimpleLabels(true);
-		plot.setLabelBackgroundPaint(null);
-		plot.setLabelOutlinePaint(null);
-		plot.setLabelShadowPaint(null);
-		plot.setLabelPaint(Color.WHITE);
+		plot.setShadowPaint          ( null );
+		plot.setBackgroundAlpha      ( 0.0f ) ;
+		plot.setSimpleLabels         ( true );
+		plot.setLabelBackgroundPaint ( null );
+		plot.setLabelOutlinePaint    ( null );
+		plot.setLabelShadowPaint     ( null );
+		plot.setLabelPaint           ( Color.WHITE );
 		
-		Font font = new Font("Arial", Font.BOLD, 14);
-		plot.setLabelFont( font );
-		plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{1}h"));
+		Font font = new Font ( "Arial", Font.BOLD, 14 );
+		plot.setLabelFont      ( font );
+		plot.setLabelGenerator ( new StandardPieSectionLabelGenerator ( "{1}h" ) );
 
 
-		this.add(cPanel, BorderLayout.CENTER);
-		this.setVisible(true);
+		this.add ( cPanel, BorderLayout.CENTER );
+		this.setVisible ( true );
 	}
 
-	public static void main( String[] args )
+	public static void main ( String[] args )
 	{
 		// Donnees
-		Map donnees = new LinkedHashMap<String,Double>();
-		donnees.put("TP", 56+56+12+12+0.0 );
-		donnees.put("TD", 28+28+9+9+19.5 );
-		donnees.put("CM", 28.0 );
-		/*donnees.put("A", 28.0 );
-		donnees.put("B", 28.0 );
-		donnees.put("C", 28.0 );*/
-		donnees.put("NA", 20.0 ); //((int)(165.5))-(heuresTP+heuresTD+heuresCM)
+		Map donnees = new LinkedHashMap<String,Double> ( );
+		donnees.put ( "TP", 56+56+12+12+0.0 );
+		donnees.put ( "TD", 28+28+9+9+19.5 );
+		donnees.put ( "CM", 28.0 );
+		/*donnees.put ( "A", 28.0 );
+		donnees.put ( "B", 28.0 );
+		donnees.put ( "C", 28.0 );*/
+		donnees.put ( "NA", 20.0 ); //((int)(165.5))-(heuresTP+heuresTD+heuresCM)
 		
 		// Generation
-		JFrame f = new JFrame();
-		f.setVisible(true);
-		System.out.println("Diagramme généré");
-		f.add( new PanelDiagramme(donnees, new Color(255,99,71), new Color(255,192,124)) );
-		f.pack();
+		JFrame f = new JFrame ( );
+		f.setVisible ( true );
+		System.out.println ( "Diagramme généré" );
+		f.add ( new PanelDiagramme ( donnees, new Color ( 255,99,71 ), new Color ( 255,192,124 ) ) );
+		f.pack ( );
 	}
 
-	public static List<Color> generateGradientColors(Color startColor, Color endColor, int steps)
+	public static List<Color> generateGradientColors ( Color startColor, Color endColor, int steps )
 	{
-		List<Color> gradientColors = new ArrayList<>();
+		List<Color> gradientColors = new ArrayList<> ( );
 
-		for (int i = 0; i < steps; i++)
+		for ( int i = 0; i < steps; i++ )
 		{
-			float ratio = (float) i / (float) (steps - 1);
-			int red = (int) (startColor.getRed() + ratio * (endColor.getRed() - startColor.getRed()));
-			int green = (int) (startColor.getGreen() + ratio * (endColor.getGreen() - startColor.getGreen()));
-			int blue = (int) (startColor.getBlue() + ratio * (endColor.getBlue() - startColor.getBlue()));
-			Color color = new Color(red, green, blue);
-			gradientColors.add(color);
+			float ratio = ( float ) i / ( float ) ( steps - 1 );
+			
+			int red   = ( int ) ( startColor.getRed   ( ) + ratio * ( endColor.getRed   ( ) - startColor.getRed   ( ) ) );
+			int green = ( int ) ( startColor.getGreen ( ) + ratio * ( endColor.getGreen ( ) - startColor.getGreen ( ) ) );
+			int blue  = ( int ) ( startColor.getBlue  ( ) + ratio * ( endColor.getBlue  ( ) - startColor.getBlue  ( ) ) );
+			
+			Color color = new Color ( red, green, blue );
+			gradientColors.add ( color );
 		}
 
 		return gradientColors;
