f79688ee70b9cd1b53d387860d02581fcdc67172
Astyell <etoiledeplante@gmail.com>
2023-12-13 14:38:45 +0100

    MAJ BD

--- src/astre/modele/BD.java
+++ src/astre/modele/BD.java
@@ -31,8 +31,8 @@ public class BD
 		{
 			Class.forName ( "org.postgresql.Driver" );
 			
-			co = DriverManager.getConnection ( "jdbc:postgresql://localhost:7777/sm220306", "sm220306", "mateo2705" ); //Pour alizéa
-			//co = DriverManager.getConnection ( "jdbc:postgresql://woody/sm220306", "sm220306", "mateo2705" );
+			//co = DriverManager.getConnection ( "jdbc:postgresql://localhost:7777/sm220306", "sm220306", "mateo2705" ); //Pour alizéa
+			co = DriverManager.getConnection ( "jdbc:postgresql://woody/sm220306", "sm220306", "mateo2705" );
 		} 
 		catch ( ClassNotFoundException e ) 
 		{
