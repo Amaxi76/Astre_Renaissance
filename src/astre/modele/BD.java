0db31eaff969d87b96adf208351fe8a2a21e72a4
Amaxi76 <lesterlinpro@gmail.com>
2023-12-13 11:35:04 +0100

    Ajout de la validation dans le PanelSemestre

--- src/astre/modele/BD.java
+++ src/astre/modele/BD.java
@@ -101,6 +101,8 @@ public class BD
 
 	public List<ModuleIUT> getModules ( int numeroSemestre )
 	{
+		//TODO: refaire le compteur
+		
 		ArrayList<ModuleIUT> ensModules = new ArrayList<> ( );
 		
 		String REQUETE = "SELECT * FROM f_selectModulesIUTParSemestre(?)";
@@ -116,7 +118,7 @@ public class BD
 
 			while ( rs.next ( ) ) 
 			{
-				int iS = 5;
+				int iS = 6;
 				int iM = 1;
 
 				Semestre   semestre   = new Semestre   ( rs.getInt ( iS++ ), rs.getInt ( iS++ ), rs.getInt ( iS++ ), rs.getInt ( iS++ ), rs.getInt ( iS ) );
@@ -124,7 +126,9 @@ public class BD
 				Map<Heure, Integer> hmHeuresPn         = this.getHeures ( rs.getString ( 1 ), 'P' );
 				Map<Heure, Integer> hmHeuresRepartiees = this.getHeures ( rs.getString ( 1 ), 'R' );
 				
-				ModuleIUT  moduleIUT =  new ModuleIUT ( semestre, rs.getString ( 4 ), rs.getString ( iM++ ), rs.getString ( iM++ ), rs.getString ( iM ), hmHeuresPn, hmHeuresRepartiees );
+				ModuleIUT  moduleIUT =  new ModuleIUT ( semestre, rs.getString ( 4 ), rs.getString ( iM++ ), rs.getString ( iM++ ), rs.getString ( iM++ ), rs.getBoolean ( 5 ), hmHeuresPn, hmHeuresRepartiees );
+
+				System.out.println(moduleIUT.estValide());
 
 				ensModules.add ( moduleIUT );
 			}
