a7ce4cdb06576925a9aa560186a48ed46fa81356
Astyell <etoiledeplante@gmail.com>
2023-12-13 09:50:20 +0100

    Fix insertion

--- database/insertion_debug.sql
+++ database/insertion_debug.sql
@@ -67,12 +67,12 @@ INSERT INTO Intervient (Id_Intervenant, nomHeure, Code_ModuleIUT, nbSemaine, nbG
 (3, 'REH', 'S6.ST', NULL, NULL,   12, NULL         );
   
 INSERT INTO Horaire VALUES
-('TP' , 'R1.01', 85, 2, 12),
-('TD' , 'R1.01', 30, 1, 11),
-('CM' , 'R1.01',  5, 2,  5),
-('SAE', 'S5.01', 60, 3,  3),
-('REH', 'S2.05',  2, 2,  1),
-('Tut', 'S6.ST',  6, 4,  3);
+('TP' , 'R1.01', 85, 0, 12),
+('TD' , 'R1.01', 30, 0, 11),
+('CM' , 'R1.01',  5, 0,  5),
+('SAE', 'S5.01', 60, 0,  3),
+('REH', 'S2.05',  2, 0,  1),
+('Tut', 'S6.ST',  6, 0,  3);
 
 
 
