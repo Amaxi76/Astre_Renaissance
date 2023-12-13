ff5c361618e3a6d2e217a251b8e8ab972d131318
Astyell <etoiledeplante@gmail.com>
2023-12-13 09:49:42 +0100

    o

--- database/insertion_debug.sql
+++ database/insertion_debug.sql
@@ -60,9 +60,9 @@ INSERT INTO Heure VALUES
 ('Tut', 1   );
 
 INSERT INTO Intervient (Id_Intervenant, nomHeure, Code_ModuleIUT, nbSemaine, nbGroupe, nbHeure, commentaire) VALUES
-(1,  'TD', 'R5.03',    6,    2, NULL, NULL         ),
-(1,  'TP', 'R1.01',    8,    1, NULL, NULL         ),
-(2,  'TP', 'R1.01',    8,    1, NULL, 'commentaire'),
+(1,  'TD', 'R5.03',    6,    2,    5, NULL         ),
+(1,  'TP', 'R1.01',    8,    1,    6, NULL         ),
+(2,  'TP', 'R1.01',    8,    1,    9, 'commentaire'),
 (3, 'Tut', 'S2.05', NULL, NULL,    9, NULL         ),
 (3, 'REH', 'S6.ST', NULL, NULL,   12, NULL         );
   
