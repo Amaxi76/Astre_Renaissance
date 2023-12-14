cf8e6b1adb51d7bb02c02302bf84105e5e5ee7e2
Mateo Sa <mateo.sa@etu.univ-lehavre.fr>
2023-12-14 11:31:47 +0100

    Génération du csv

--- src/astre/modele/GenerateurFichier.java
+++ src/astre/modele/GenerateurFichier.java
@@ -12,32 +12,33 @@ import java.io.IOException;
 
 public class GenerateurFichier
 {
-	public static void recapTtInter()
+	public static void recapTtInter ( )
 	{
         String chemin = "./fichierGenerer/recapTtIntervenant.csv";
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(chemin)))
+        try ( BufferedWriter ecrivain = new BufferedWriter ( new FileWriter ( chemin ) ) )
 		{
             // Écrivez les en-têtes du CSV
 			String entete = "Catégorie, Nom, Prénom, Service dû, Maximum heure, Coefficient TP, S1, S3, S5, sTot, S2, S4, S6, sTot, Total";
 
-            writer.write(entete);
-            writer.newLine();
+            ecrivain.write ( entete );
+            ecrivain.newLine ( );
 			
-			BD bd = BD.getInstance(); //A changer ??
-			Object[][] elem = bd.getIntervenantsTableau();
-			for(int i = 0; i < elem.length; i++)
+			BD bd = BD.getInstance ( ); //A changer ??
+			Object[][] elem = bd.getIntervenantsTableau ( );
+			for ( int i = 0; i < elem.length; i++ )
 			{
-				for(int j=1; j < elem[0].length; j++)
+				for ( int j=1; j < elem[0].length; j++ )
 				{
-					writer.write(elem[i][j].toString() + ",");
+					ecrivain.write ( elem[i][j].toString ( ) + "," );
 				}
-            	writer.newLine();
+            	ecrivain.newLine ( );
 			}
 
-            System.out.println("Fichier CSV créé avec succès.");
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println ( "Fichier CSV créé avec succès." );
+        } catch ( IOException e )
+		{
+            e.printStackTrace ( );
         }
 	}
 }
