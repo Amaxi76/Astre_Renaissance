e3aef7549c9ba73a23eca49249ac624c1ac78d7e
Amaxi76 <lesterlinpro@gmail.com>
2023-12-14 16:26:09 +0100

    Gestion des conflits

--- database/insertion_debug.sql
+++ database/insertion_debug.sql
@@ -54,7 +54,6 @@ INSERT INTO Intervenant (nom, prenom, hService, hMax, Id_Contrat) VALUES
 ('De Balzac'     , 'Honoré' ,  65,  89, 3 ),
 ('Toriyama'      , 'Akira'  , 487, 478, 2 );
 
-<<<<<<< HEAD
 INSERT INTO Heure VALUES
 ('TP'   , 1   ),
 ('TD'   , 1   ),
@@ -63,15 +62,6 @@ INSERT INTO Heure VALUES
 ('SAE'  , 1   ),
 ('PONCT', 1   ),
 ('Tut'  , 1   );
-=======
-INSERT INTO Heure (nomHeure, coeffTD) VALUES
-('TP' , 1   ),
-('TD' , 1   ),
-('CM' , 1.5 ),
-('REH', 1   ),
-('SAE', 1   ),
-('Tut', 1   );
->>>>>>> d8784a63d8fab3f4f48fa039f8f98778ca1a7dba
 
 INSERT INTO Intervient (Id_Intervenant, Id_Heure, Code_ModuleIUT, nbSemaine, nbGroupe, nbHeure, commentaire) VALUES
 (1,  2, 'R5.03',    6,    2,    5, NULL         ),
