ee402a0a91ddc6244092506c60348231f2c7cec1
Astyell <etoiledeplante@gmail.com>
2023-12-18 17:11:00 +0100

    MAJ BD Historique

--- database/creation_ASTRE.sql
+++ database/creation_ASTRE.sql
@@ -114,6 +114,58 @@ CREATE TABLE Historique
 (
    Id_Historique      SERIAL,
    dateModification   TIMESTAMP     NOT NULL,
-   String             VARCHAR(150)  NOT NULL,
+   commentaire        VARCHAR(150)  NOT NULL,
+
    PRIMARY KEY(Id_Historique)
 );
+
+/* Création des trigger */
+
+-- Pour les intervenants
+CREATE TRIGGER tr_update_Intervenant
+AFTER INSERT OR UPDATE OR DELETE
+ON Intervenant
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour les Semestre
+CREATE TRIGGER tr_update_Semestre
+AFTER INSERT OR UPDATE OR DELETE
+ON Semestre
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour les Contrat
+CREATE TRIGGER tr_update_Contrat
+AFTER INSERT OR UPDATE OR DELETE
+ON Contrat
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour les heures
+CREATE TRIGGER tr_update_Heure
+AFTER INSERT OR UPDATE OR DELETE
+ON Heure
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour les modules
+CREATE TRIGGER tr_update_ModuleIUT
+AFTER INSERT OR UPDATE OR DELETE
+ON ModuleIUT
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour intervient
+CREATE TRIGGER tr_update_Intervient
+AFTER INSERT OR UPDATE OR DELETE
+ON Intervient
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
+
+-- Pour horaire
+CREATE TRIGGER tr_update_Horaire
+AFTER INSERT OR UPDATE OR DELETE
+ON Horaire
+FOR EACH ROW
+EXECUTE FUNCTION f_update_historique();
