70e02de790d68b8ac7386731fefba8b6bda501ae
Astyell <etoiledeplante@gmail.com>
2023-12-20 09:55:43 +0100

    Ajout de la vue v_Intervenant

--- database/creation_ASTRE.sql
+++ database/creation_ASTRE.sql
@@ -169,3 +169,29 @@ AFTER INSERT OR UPDATE OR DELETE
 ON Horaire
 FOR EACH ROW
 EXECUTE FUNCTION f_update_historique_Horaire();
+
+/* Création de vue */
+
+CREATE VIEW v_Intervenant AS
+SELECT
+      Id_Intervenant,
+      nomContrat,
+      nom,
+      prenom,
+      hService,
+      hMax,
+      ratioTP,
+      f_selectNBHeureParSemestre(1, Id_Intervenant) AS s1,
+      f_selectNBHeureParSemestre(3, Id_Intervenant) AS s3,
+      f_selectNBHeureParSemestre(5, Id_Intervenant) AS s5,
+      f_selectNBHeureParSemestreImpair(Id_Intervenant) AS totImp,
+      f_selectNBHeureParSemestre(2, Id_Intervenant) AS s2,
+      f_selectNBHeureParSemestre(4, Id_Intervenant) AS s4,
+      f_selectNBHeureParSemestre(6, Id_Intervenant) AS s6,
+      f_selectNBHeureParSemestrePair(Id_Intervenant) AS totPai,
+      f_selectNBHeureParSemestreTot(Id_Intervenant) AS total
+FROM
+      Intervenant i
+      JOIN Contrat c ON i.Id_Contrat = c.Id_Contrat
+ORDER BY
+      Id_intervenant ASC;
